71817b79b9ab69c3332353e5672097f1e62e9866
AIbdullah <mabdullahsaqib2004@gmail.com>
2024-07-17 22:35:55 +0500

    test to image to 3d conversion

--- src/txt to image.py
+++ src/txt to image.py
@@ -1,14 +1,15 @@
-import requests
+# import requests
+#
+# url = "https://ai-text-to-image-generator-api.p.rapidapi.com/realistic"
+#
+# payload = { "inputs": "A teddy bear in the storm." }
+# headers = {
+# 	"x-rapidapi-key": "",
+# 	"x-rapidapi-host": "",
+# 	"Content-Type": "application/json"
+# }
+#
+# response = requests.post(url, json=payload, headers=headers)
+#
+# print(response.json())
 
-url = "https://ai-text-to-image-generator-api.p.rapidapi.com/realistic"
-
-payload = { "inputs": "A teddy bear in the storm." }
-headers = {
-	"x-rapidapi-key": "",
-	"x-rapidapi-host": "",
-	"Content-Type": "application/json"
-}
-
-response = requests.post(url, json=payload, headers=headers)
-
-print(response.json())
