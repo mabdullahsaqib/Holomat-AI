ee6bb8e4ce8b945bdebf794b7bffbeb1146a8b72
AIbdullah <mabdullahsaqib2004@gmail.com>
2024-07-18 05:54:43 +0500

    trying more methods

--- src/txt to image.py
+++ src/txt to image.py
@@ -1,15 +1,28 @@
-# import requests
-#
-# url = "https://ai-text-to-image-generator-api.p.rapidapi.com/realistic"
-#
-# payload = { "inputs": "A teddy bear in the storm." }
-# headers = {
-# 	"x-rapidapi-key": "",
-# 	"x-rapidapi-host": "",
-# 	"Content-Type": "application/json"
-# }
-#
-# response = requests.post(url, json=payload, headers=headers)
-#
-# print(response.json())
+import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+limewireapi_key = os.getenv("LIMEWIRE_API_KEY")
+
+url = "https://api.limewire.com/api/image/generation"
+
+payload = {
+  "prompt": "a ninja, black clothes, holding a katana, anime style",
+  "aspect_ratio": "1:1"
+}
+
+headers = {
+  "Content-Type": "application/json",
+  "X-Api-Version": "v1",
+  "Accept": "application/json",
+  "Authorization": f"Bearer {limewireapi_key}"
+}
+
+response = requests.post(url, json=payload, headers=headers)
+
+data = response.json()
+print(data)
+# image_url = response.json()["data"]["asset_url"]
+# print(image_url)
